53d76f1256a4bc56c8114cbcd406778a98fd9ec2
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2016-12-09 11:19:12 +0100

    fixed path delimiters

--- test/file-utils.spec.js
+++ test/file-utils.spec.js
@@ -9,7 +9,7 @@ describe('FileUtils', () => {
 
   it('should throw an error if file is not a directory', () => {
     //given
-    let dir = __dirname + '\\file-utils-test\\test3';
+    let dir = __dirname + '/file-utils-test/test3';
 
     //when
     expect(() => readDirectory(dir, {})).toThrow(dir + ' does not exist');
@@ -17,7 +17,7 @@ describe('FileUtils', () => {
 
   it('should read all files in a directory', () => {
     //given
-    let dir = __dirname + '\\file-utils-test\\test1';
+    let dir = __dirname + '/file-utils-test/test1';
 
     //given
     let start = 1;
@@ -25,8 +25,8 @@ describe('FileUtils', () => {
     //when
     readDirectory(dir, {}, (text, file) => {
       expect(text).toBe('test content ' + start);
-      expect(file.includes('\\file-utils-test\\test1')).toBeTruthy();
-      expect(file.includes('\\file' + start + '.txt')).toBeTruthy();
+      expect(file.includes('/file-utils-test/test1') || file.includes('\\file-utils-test\\test1')).toBeTruthy();
+      expect(file.includes('/file' + start + '.txt') || file.includes('\\file' + start + '.txt')).toBeTruthy();
       start++;
     });
 
@@ -35,7 +35,7 @@ describe('FileUtils', () => {
 
   it('should read files with specific extensions in a directory', () => {
     //given
-    let dir = __dirname + '\\file-utils-test\\test1';
+    let dir = __dirname + '/file-utils-test/test1';
 
     //given
     let start = 1;
@@ -43,8 +43,8 @@ describe('FileUtils', () => {
     //when
     readDirectory(dir, {}, (text, file) => {
       expect(text).toBe('test content ' + start);
-      expect(file.includes('\\file-utils-test\\test1')).toBeTruthy();
-      expect(file.includes('\\file' + start + '.txt')).toBeTruthy();
+      expect(file.includes('/file-utils-test/test1') || file.includes('\\file-utils-test\\test1')).toBeTruthy();
+      expect(file.includes('/file' + start + '.txt') || file.includes('\\file' + start + '.txt')).toBeTruthy();
       start++;
     });
 
@@ -53,7 +53,7 @@ describe('FileUtils', () => {
 
   it('should read files with specific extensions in a directory', () => {
     //given
-    let dir = __dirname + '\\file-utils-test\\test2';
+    let dir = __dirname + '/file-utils-test/test2';
     let extensions = ['.txt1', '.txt2'];
 
     //given
@@ -62,8 +62,8 @@ describe('FileUtils', () => {
     //when
     readDirectory(dir, {extensions: extensions}, (text, file) => {
       expect(text).toBe('test content ' + start);
-      expect(file.includes('\\file-utils-test\\test2')).toBeTruthy();
-      expect(file.includes('\\file' + start + '.txt' + start)).toBeTruthy();
+      expect(file.includes('/file-utils-test/test2') || file.includes('\\file-utils-test\\test2')).toBeTruthy();
+      expect(file.includes('/file' + start + '.txt' + start) || file.includes('\\file' + start + '.txt' + start)).toBeTruthy();
       start++;
     });
 
